33795bd920ed84c2cfdea52f908cff908422c371
Muthukalaiyarasi-Thirumalaisamy <muthukalai2018@gmail.com>
2024-12-23 23:25:14 +0530

    Your commit message

--- src/test/java/AppHooks/ApplicationHooks.java
+++ src/test/java/AppHooks/ApplicationHooks.java
@@ -6,6 +6,11 @@ import org.openqa.selenium.OutputType;
 import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
 
+import com.aventstack.extentreports.ExtentReports;
+import com.aventstack.extentreports.ExtentTest;
+import com.aventstack.extentreports.Status;
+import com.aventstack.extentreports.reporter.ExtentSparkReporter;
+import com.aventstack.extentreports.reporter.configuration.Theme;
 import com.qa.factory.DriverFactory;
 import com.qa.util.ConfigReader;
 
@@ -15,38 +20,78 @@ import io.cucumber.java.Scenario;
 
 public class ApplicationHooks {
 
-	private DriverFactory driverFactory;
-	private WebDriver driver;
-	private ConfigReader configReader;
-	 Properties prop;
+    private DriverFactory driverFactory;
+    private WebDriver driver;
+    private ConfigReader configReader;
+    private Properties prop;
+    private static ExtentReports extent; // ExtentReports instance
+    private ExtentTest test;             // ExtentTest instance for logging
 
-	@Before(order = 0)
-	public void getProperty() {
-		configReader = new ConfigReader();
-		prop = configReader.init_prob();
-	}
+    // Static block to initialize ExtentReports
+    static {
+        ExtentSparkReporter sparkReporter = new ExtentSparkReporter("test-output/SparkReport/Spark.html");
+        sparkReporter.config().setTheme(Theme.DARK);
+        sparkReporter.config().setDocumentTitle("Test Automation Report");
+        sparkReporter.config().setReportName("Cucumber Test Execution");
 
-	@Before(order = 1)
-	public void launchBrowser() {
-		String browserName = prop.getProperty("browser");
-		driverFactory = new DriverFactory();
-		driver = driverFactory.init_driver(browserName);
-		
-	}
+        extent = new ExtentReports();
+        extent.attachReporter(sparkReporter);
 
-	@After(order = 0)
-	public void quitBrowser() {
-		driver.quit();
-	}
+        // Add System Information
+        extent.setSystemInfo("OS", "Windows");
+        extent.setSystemInfo("User", "Muthukalaiyarasi Thirumalaisamy");
+        extent.setSystemInfo("Role", "Test Engineer");
+        extent.setSystemInfo("Team", "Simplification Two Pot/ QA");
+        extent.setSystemInfo("AppName", "Cucumber Automation for Swag Labs");
+    }
 
-	@After(order = 1)
-	public void tearDown(Scenario scenario) {
-		if (scenario.isFailed()) {
-			// take screenshot:
-			String screenshotName = scenario.getName().replaceAll(" ", "_");
-			byte[] sourcePath = ((TakesScreenshot) driver).getScreenshotAs(OutputType.BYTES);
-			scenario.attach(sourcePath, "image/png", screenshotName);
+    @Before(order = 0)
+    public void getProperty() {
+        configReader = new ConfigReader();
+        prop = configReader.init_prob();
+    }
 
-		}
-	}
+    @Before(order = 1)
+    public void launchBrowser(Scenario scenario) {
+        String browserName = prop.getProperty("browser");
+        driverFactory = new DriverFactory();
+        driver = driverFactory.init_driver(browserName);
+
+        // Initialize ExtentTest for this scenario
+        test = extent.createTest(scenario.getName());
+    }
+
+    @After(order = 1)
+    public void tearDown(Scenario scenario) {
+        if (scenario.isFailed()) {
+            try {
+                // Capture screenshot as a Base64 string
+                String base64Screenshot = ((TakesScreenshot) driver).getScreenshotAs(OutputType.BASE64);
+
+                // Attach screenshot to Cucumber report
+                String screenshotName = scenario.getName().replaceAll(" ", "_");
+                scenario.attach(base64Screenshot.getBytes(), "image/png", screenshotName);
+
+                // Log the failure in Extent report and attach the screenshot
+                test.log(Status.FAIL, "Test Failed. Screenshot below:");
+                test.addScreenCaptureFromBase64String(base64Screenshot, "Failed Step Screenshot");
+            } catch (Exception e) {
+                test.log(Status.FAIL, "Failed to attach screenshot due to exception: " + e.getMessage());
+            }
+        } else {
+            test.log(Status.PASS, "Test Passed.");
+        }
+    }
+
+    @After(order = 0)
+    public void quitBrowser() {
+        driver.quit();
+    }
+
+    // Static method to flush the report at the end of test execution
+    public static void flushReports() {
+        if (extent != null) {
+            extent.flush();
+        }
+    }
 }
--- src/test/java/com/TestRunner/TestRunner.java
+++ src/test/java/com/TestRunner/TestRunner.java
@@ -1,18 +1,28 @@
 package com.TestRunner;
 
+import org.junit.AfterClass;
 import org.junit.runner.RunWith;
 
+import AppHooks.ApplicationHooks;
 import io.cucumber.junit.Cucumber;
 import io.cucumber.junit.CucumberOptions;
 
 @RunWith(Cucumber.class)
 @CucumberOptions(
-		features="src/test/resources/Features/Products.feature",
-		glue={"stepDefinition","AppHooks"},
-		plugin="pretty"
-		)
-
+        features = "src/test/resources/Features",
+        glue = {"stepDefinition", "AppHooks"},
+        plugin = {
+                "pretty",
+                "com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:",
+                "html:target/cucumber-report.html"
+        }
+)
 public class TestRunner {
 
-	
+
+    @AfterClass
+    public static void tearDownAll() {
+        // Flush Extent Reports after all tests
+        ApplicationHooks.flushReports();
+    }
 }
